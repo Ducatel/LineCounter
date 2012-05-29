ccd36abbe61881b481d533864f653993cbfa216c
Ducatel <hannibal@hannibal-PC.(none)>
2012-05-29 21:49:51 +0200

    Add comment in Ihm class

--- src/ihm.py
+++ src/ihm.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 # -*-coding:utf-8 -*-
+'''
+Created on 29 mai 2012
+
+@author: Davis Ducatel
+'''
 
 import sys
 from PyQt4 import QtGui
@@ -7,15 +12,26 @@ from PyQt4 import QtCore
 import LineCounter
 
 class Ihm(QtGui.QWidget):
+    '''
+    Classe de gestion de l'interface
+    Basé sur PyQt
+    '''
+    
     def __init__(self):
+        '''
+        Constructeur de l'interface graphique de 
+        l'application LineCounter 
+        '''
         super(Ihm, self).__init__()
         self.extensionFilterList=[['.h','.cpp'],['.h','.c'],['.h','.cpp','.c'],['.java'],['.php']]
         self.dirPath=QtCore.QDir('')
-        self.dirIsChoose=False
         self.initUI()
 
     
     def initUI(self):
+        '''
+        Methode qui initialise l'interface
+        '''
         layoutPrinc = QtGui.QGridLayout()
 
         layoutPrinc.addWidget(QtGui.QLabel('Dossier racine du projet: '),0,0,1,2)
@@ -43,11 +59,12 @@ class Ihm(QtGui.QWidget):
             self.fileFilter.addItem(textFileFilter)
         layoutPrinc.addWidget(self.fileFilter,8,1,1,3)
 
-        self.chooseFileButton = QtGui.QPushButton('Dossier', self)
+        self.chooseFileButton = QtGui.QPushButton('Choisir un dossier', self)
         self.chooseFileButton.clicked.connect(self.chooseFileAction)
         layoutPrinc.addWidget(self.chooseFileButton,9,0,1,2)
         
         self.goButton = QtGui.QPushButton('Compter', self)
+        self.goButton.setEnabled(False)
         self.goButton.clicked.connect(self.goButtonAction)
         layoutPrinc.addWidget(self.goButton,9,2,1,2)   
 
@@ -61,24 +78,32 @@ class Ihm(QtGui.QWidget):
         self.show()
         
     def chooseFileAction(self):
+        '''
+        Methode qui gere le click sur le bouton "choisir dossier"
+        Lance la selection du dossier
+        '''
         fileName = QtGui.QFileDialog.getExistingDirectory(self, "Choisir un fichier")
         if fileName:
             self.pathLabel.setText(fileName)
-            self.dirIsChoose=True
+            self.goButton.setEnabled(True)
             self.dirPath.setPath(fileName)
             self.progressBar.setValue(0)
             self.affichage.clear()
 
     def goButtonAction(self):
-        if self.dirIsChoose:
-            printAllFile=False
-            if self.printAllFile.checkState() == QtCore.Qt.Checked:
-                printAllFile=True
-                
-            line=LineCounter.LineCounter(str(self.dirPath.path().toUtf8())
-                                         ,printAllFile,self
-                                         ,self.extensionFilterList[self.fileFilter.currentIndex()])
-            line.compute()
+        '''
+        Methode qui gere le click sur le bouton "compter"
+        Lance le calcul
+        '''
+        printAllFile=False
+        if self.printAllFile.checkState() == QtCore.Qt.Checked:
+            printAllFile=True
+            
+        line=LineCounter.LineCounter(str(self.dirPath.path().toUtf8())
+                                     ,printAllFile,self
+                                     ,self.extensionFilterList[self.fileFilter.currentIndex()])
+        line.compute()
+        
     
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
